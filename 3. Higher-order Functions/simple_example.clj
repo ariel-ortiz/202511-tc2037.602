11fa47259c9b220333bc67a8005a20113db0fe23
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-03-13 12:01:44 -0600

    Add more code to demo more higher-order functions

--- 3. Higher-order Functions/simple_example.clj
+++ 3. Higher-order Functions/simple_example.clj
@@ -23,3 +23,38 @@
 (w 50)
 
 ((suma 20) 5)
+
+(defn composite
+  [f g]
+  (fn [x] (f (g x))))
+
+(defn f1 [x] (* 2 x))
+(defn f2 [x] (+ x 5))
+(def f3 (composite f1 f2))
+(def f4 (composite f2 f1))
+(def f5 (composite f3 f4))
+
+(f1 1)
+(f2 1)
+(f3 1)
+(f4 1)
+(f5 1)
+
+(defn z
+  [a b c d e]
+  (+ (* a b)
+     (/ c d)
+     (inc e)))
+
+(defn z-curry
+  [a]
+  (fn [b]
+    (fn [c]
+      (fn [d]
+        (fn [e]
+          (+ (* a b)
+             (/ c d)
+             (inc e)))))))
+
+(z 1 2 3 4 5)
+(((((z-curry 1) 2) 3) 4) 5)
