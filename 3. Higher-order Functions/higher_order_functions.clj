42346a4fbf33d9f724c51d619bb83f25848ab573
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-03-13 12:54:26 -0600

    Add code to solve problems 1, 2, and 3 of problem set 4.

---
+++ 3. Higher-order Functions/higher_order_functions.clj
@@ -0,0 +1,82 @@
+(ns higher-order-functions
+  (:require [clojure.test :refer [deftest is run-tests]])
+  (:require [clojure.algo.generic.math-functions
+             :refer [approx=]]))
+
+; Problema 1
+(defn argswap
+  [f]
+  (fn [x y] (f y x)))
+
+; Problema 2
+(defn there-exists-one
+  [pred? s]
+  (= 1 (count (filter pred? s))))
+
+; Problema 3
+(defn linear-search
+  [vct x eq-fun]
+  (loop [i 0]
+    (cond
+      (= i (count vct))   nil
+      (eq-fun x (vct i))  i
+      :else               (recur (inc i)))))
+
+(linear-search [4 8 15 16 23 42]
+               24
+               #(<= (abs (- %1 %2)) 1))
+
+
+(deftest test-argswap
+  (is (= '(2 1)
+         ((argswap list) 1 2)))
+  (is (= -7
+         ((argswap -) 10 3)))
+  (is (= 1/4
+         ((argswap /) 8 2)))
+  (is (= '((4 5 6) 1 2 3)
+         ((argswap cons) '(1 2 3) '(4 5 6))))
+  (is (= '(1 0 4 25 100)
+         ((argswap map) '(-1 0 2 5 10) #(* % %)))))
+
+(deftest test-there-exists-one
+  (is (not (there-exists-one pos?
+                             ())))
+  (is (there-exists-one pos?
+                        '(-1 -10 4 -5 -2 -1)))
+  (is (there-exists-one neg?
+                        '(-1)))
+  (is (not (there-exists-one symbol?
+                             '(4 8 15 16 23 42))))
+  (is (there-exists-one symbol?
+                        '(4 8 15 sixteen 23 42))))
+
+(deftest test-linear-search
+  (is (nil? (linear-search [] 5 =)))
+  (is (= 0 (linear-search [5] 5 =)))
+  (is (= 4 (linear-search
+             [48 77 30 31 5 20 91 92
+              69 97 28 32 17 18 96]
+             5
+             =)))
+  (is (= 3 (linear-search
+             ["red" "blue" "green" "black" "white"]
+             "black"
+             identical?)))
+  (is (nil? (linear-search
+              [48 77 30 31 5 20 91 92
+               69 97 28 32 17 18 96]
+              96.0
+              =)))
+  (is (= 14 (linear-search
+              [48 77 30 31 5 20 91 92
+               69 97 28 32 17 18 96]
+              96.0
+              ==)))
+  (is (= 8 (linear-search
+             [48 77 30 31 5 20 91 92
+              69 97 28 32 17 18 96]
+             70
+             #(<= (abs (- %1 %2)) 1)))))
+
+(run-tests)
