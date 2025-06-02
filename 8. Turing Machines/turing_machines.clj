42dc41d73a6d66e71d63124207f6501c13fe7e57
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-06-02 12:55:02 -0600

    Add code to solve problems 1, 2 and 3 of problem set 9

--- 8. Turing Machines/turing_machines.clj
+++ 8. Turing Machines/turing_machines.clj
@@ -12,10 +12,6 @@
   [self ^Writer writer]
   (.write writer (str self)))
 
-; (def ejemplo (->Tape "aaaa" \c "bbbbb"))
-
-; (println ejemplo)
-
 (defn make-tape
   ([s]
    (let [result (drop-while #(= % \_) s)]
@@ -44,9 +40,95 @@
                       (rest right))
     (throw (ex-info (str "Bad direction: " direction) {}))))
 
+(defn accepts
+  [{:keys [initial-state accept-states transitions]} input]
+  (loop [tape (make-tape input)
+         current-state initial-state]
+    (if (contains? accept-states current-state)
+      (str tape)
+      (if-let [[write-symbol direction new-state]
+               ((transitions current-state) (.head tape))]
+        (recur (shift-head (write-tape tape write-symbol) direction)
+               new-state)
+        nil))))
 
-;(def example (make-tape "aaaa" \b "ccccc"))
+; Problema 1
+(def tm-1 (->TM :q0
+                #{:q2}
+                {:q0 {\a [\a :right :q1]
+                      \_ [\_ :left :q2]}
+                 :q1 {\a [\a :right :q0]}}))
 
-; (shift-head example :right)
+(deftest test-problem1
+  (is (= "[_]"
+         (accepts tm-1 "")))
+  (is (= "a[a]"
+         (accepts tm-1 "aa")))
+  (is (= "aaaaaaa[a]"
+         (accepts tm-1 "aaaaaaaa")))
+  (is (= "aaaaaaaaaaaaaaaaaaaaaaaaa[a]"
+         (accepts tm-1 "aaaaaaaaaaaaaaaaaaaaaaaaaa")))
+  (is (nil? (accepts tm-1 "a")))
+  (is (nil? (accepts tm-1 "aaa")))
+  (is (nil? (accepts tm-1 "aaaaaaa")))
+  (is (nil? (accepts tm-1 "aaaaaaaaaaaaaaaaaaaaaaaaa"))))
+
+; Problema 2
+(def tm-2 (->TM :q0
+                #{:q3}
+                {:q0 {\0 [\0 :right :q1]
+                      \1 [\1 :right :q2]}
+                 :q1 {\1 [\1 :right :q1]
+                      \_ [\_ :left :q3]}
+                 :q2 {\0 [\0 :right :q2]
+                      \_ [\_ :left :q3]}}))
+
+(deftest test-problem2
+  (is (= "[0]"
+         (accepts tm-2 "0")))
+  (is (= "[1]"
+         (accepts tm-2 "1")))
+  (is (= "1[0]"
+         (accepts tm-2 "10")))
+  (is (= "0111111111[1]"
+         (accepts tm-2 "01111111111")))
+  (is (nil? (accepts tm-2 "")))
+  (is (nil? (accepts tm-2 "00")))
+  (is (nil? (accepts tm-2 "100000000001")))
+  (is (nil? (accepts tm-2 "10011010100101011"))))
+
+; Problema 3
+(def tm-3 (->TM :q0
+                #{:q3}
+                {:q0 {\0 [\0 :right :q0]
+                      \1 [\1 :right :q0]
+                      \_ [\_ :left :q1]}
+                 :q1 {\0 [\1 :right :q2]
+                      \1 [\0 :left :q1]
+                      \_ [\1 :right :q2]}
+                 :q2 {\0 [\0 :right :q2]
+                      \_ [\_ :left :q3]}}))
+
+(deftest test-problem3
+  (is (= "[1]"
+         (accepts tm-3 "0")))
+  (is (= "1[0]"
+         (accepts tm-3 "1")))
+  (is (= "1[1]"
+         (accepts tm-3 "10")))
+  (is (= "10[0]"
+         (accepts tm-3 "11")))
+  (is (= "100[1]"
+         (accepts tm-3 "1000")))
+  (is (= "10101011[0]"
+         (accepts tm-3 "101010101")))
+  (is (= "000000000[1]"
+         (accepts tm-3 "0000000000")))
+  (is (= "11111000[0]"
+         (accepts tm-3 "111101111")))
+  (is (= "101001101[1]"
+         (accepts tm-3 "1010011010")))
+  (is (= "1000000000000000[0]"
+         (accepts tm-3 "1111111111111111"))))
 
 (run-tests)
